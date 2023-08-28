9e9858b6db690082e4f58e8460f2df1b60c8ba3b
Aleksi Hemmilä <aleksi.hemmila@alfame.com>
2023-08-29 00:33:17 +0300

    osa3 tehtävät 3.12 - 3.18

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const Contact = require('./models/contact')
 const express = require('express')
 const app = express()
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 var morgan = require('morgan')
+
+
 
 morgan.token('bodyMessage',(req,res) => {  
   return JSON.stringify(req.body);
@@ -15,104 +18,103 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    phone: "040-123456",    
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    phone: "39-44-5323523",    
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    phone: "12-43-234345",    
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    phone: "39-23-6423122",    
-  }
-]
-
-
-
 app.get('/', (req, res) => {
-  res.send('<h1>Phonebook!</h1>')
+  res.send('<h1>Front is not running!</h1>')
 })
 
-app.get('/info', (req, res) => {
-  const infoPage = GetInfoPage();
+app.get('/info', async (req, res, next) => {
+  try{
+  const infoPage = await GetInfoPage();  
   res.send(infoPage)
+  } catch(error){
+    next(error)
+  }
+  
 })
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
+app.get('/api/persons', (req, res, next) => {
+  Contact.find({}).then(con => {
+    res.json(con)
+  })
+  .catch(error => next(error))  
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if(person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }  
+app.get('/api/persons/:id', (request, response, next) => {
+  Contact.findById(request.params.id)
+  .then(con=>{
+    if(con) {
+      response.json(con)
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))          
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Contact.findOneAndDelete(request.params.id)
+  .then(result=> {
+    response.status(204).end()
+  })
+  .catch(error=>next(error)) 
+  
 })
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body  
 
-app.post('/api/persons', (request, response) => {
+  const contact = {
+    name: body.name,
+    phone: body.phone
+  }
+  Contact.findByIdAndUpdate(request.params.id, contact, {new:true})
+    .then(updatedContact=>{
+      response.json(updatedContact)
+    })
+    .catch(error=>next(error))                    
+})
+app.post('/api/persons', (request, response,next) => {
   const body = request.body
 
   if (!body.name || !body.phone) {
     return response.status(400).json({ 
       error: 'name or phone missing!' 
     })
-  }
-  if (persons.find(person=>person.name === body.name))
-  {
-    return response.status(400).json({
-      error: 'name duplicates are not allowed!'
-    })  
-  }
-  const person = CreateNewPerson(body)  
-  persons = persons.concat(person)
-  response.json(person)
+  }  
+
+  const contact = new Contact({
+    name: body.name,
+    phone: body.phone
+  })
+
+  contact.save().then(savedContact => {
+    response.json(savedContact)
+  })
+  .catch(error=>next(error))
 })
 
-const GetInfoPage =() => {
+const GetInfoPage =async () => {  
+  try{
+  const count = await Contact.countDocuments({})
+
   const info=`
     <div>
-      <p>Phonebook has info for ${persons.length} people</p>
+      <p>Phonebook has info for ${count} people</p>
       <p>${new Date()}</p>
     </div>
     `
-  return info;
-}
-const CreateNewPerson = (body) =>{
-  const min = 10000000;
-  const max = 100000000;
-  const randomInt = Math.floor(Math.random() * (max-min+1))+min
-  return {
-    id: generateId(),
-    name: body.name,
-    phone: body.phone || `${randomInt}`,   
+  return info; 
+  } catch(error) {
+    throw error;
   }
-  
+}
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
 }
 
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
-}
+app.use(errorHandler)
