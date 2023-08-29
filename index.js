b2a09f020c4ff99509468f2fe9c9c10f249618cc
Aleksi Hemmilä <aleksi.hemmila.alfame@ahti.cloud>
2023-08-29 12:55:02 +0300

    osa3, tehtävä 3.22

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const app = express()
 app.use(express.static('dist'))
 app.use(express.json())
 var morgan = require('morgan')
-
-
-
-morgan.token('bodyMessage',(req,res) => {  
-  return JSON.stringify(req.body);
+//eslint-disable-next-line
+morgan.token('bodyMessage', (req, res) => {
+  return JSON.stringify(req.body)
 })
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :bodyMessage'))
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :bodyMessage'
+  )
+)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
@@ -23,97 +25,102 @@ app.get('/', (req, res) => {
 })
 
 app.get('/info', async (req, res, next) => {
-  try{
-  const infoPage = await GetInfoPage();  
-  res.send(infoPage)
-  } catch(error){
+  try {
+    const infoPage = await GetInfoPage()
+    res.send(infoPage)
+  } catch (error) {
     next(error)
   }
-  
 })
 
 app.get('/api/persons', (req, res, next) => {
-  Contact.find({}).then(con => {
-    res.json(con)
-  })
-  .catch(error => next(error))  
+  Contact.find({})
+    .then((con) => {
+      res.json(con)
+    })
+    .catch((error) => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
   Contact.findById(request.params.id)
-  .then(con=>{
-    if(con) {
-      response.json(con)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))          
+    .then((con) => {
+      if (con) {
+        response.json(con)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
+
   Contact.findOneAndDelete(request.params.id)
-  .then(result=> {
-    response.status(204).end()
-  })
-  .catch(error=>next(error)) 
-  
+  //eslint-disable-next-line
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 app.put('/api/persons/:id', (request, response, next) => {
-  const {name, phone} = request.body  
+  const { name, phone } = request.body
 
   Contact.findByIdAndUpdate(
-    request.params.id, 
-    {name, phone}, 
-    {new:true, runValidators: true, context: 'query'}
+    request.params.id,
+    { name, phone },
+    { new: true, runValidators: true, context: 'query' }
   )
-    .then(updatedContact=>{
+    .then((updatedContact) => {
       response.json(updatedContact)
     })
-    .catch(error=>next(error))                    
+    .catch((error) => next(error))
 })
-app.post('/api/persons', (request, response,next) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.phone) {
-    return response.status(400).json({ 
-      error: 'name or phone missing!' 
+    return response.status(400).json({
+      error: 'name or phone missing!',
     })
-  }  
+  }
 
   const contact = new Contact({
     name: body.name,
-    phone: body.phone
+    phone: body.phone,
   })
 
-  contact.save().then(savedContact => {
-    response.json(savedContact)
-  })
-  .catch(error=>next(error))
+  contact
+    .save()
+    .then((savedContact) => {
+      response.json(savedContact)
+    })
+    .catch((error) => next(error))
 })
 
-const GetInfoPage =async () => {  
-  try{
-  const count = await Contact.countDocuments({})
+const GetInfoPage = async () => {
+  /* eslint-disable*/
+  try {
+    const count = await Contact.countDocuments({})
 
-  const info=`
+    const info = `
     <div>
       <p>Phonebook has info for ${count} people</p>
       <p>${new Date()}</p>
     </div>
     `
-  return info; 
-  } catch(error) {
-    throw error;
+    return info
+  } catch (error) {
+    throw error
   }
+  /* eslint-enable */
 }
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } else if(error.name === 'ValidationError') {
-    return response.status(400).json({error: error.message})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
--- models/contact.js
+++ models/contact.js
@@ -4,11 +4,11 @@ mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
-
 console.log('connecting to', url)
-mongoose.connect(url)
-
-  .then(result => {
+mongoose
+  .connect(url)
+//eslint-disable-next-line
+  .then((result) => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
@@ -16,32 +16,31 @@ mongoose.connect(url)
   })
 
 const contactSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      minlength: 3,
-      required:true
-    },
-    phone: {
-      type:String,
-      minlength:8,
-      
-      validate: {
-        validator: function(pnum) {
-          return /^\d{2,3}-\d{7,8}/.test(pnum);
-        },
-        message: props => `${props.value} is not a valid phone number`
-      },
-      required:[true, 'nimen kanssa on oltava puhelinnumero'],
-    }
-  })
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  phone: {
+    type: String,
+    minlength: 8,
 
-  contactSchema.set('toJSON', {
+    validate: {
+      validator: function (pnum) {
+        return /^\d{2,3}-\d{7,8}/.test(pnum)
+      },
+      message: (props) => `${props.value} is not a valid phone number`,
+    },
+    required: [true, 'nimen kanssa on oltava puhelinnumero'],
+  },
+})
+
+contactSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  }
+  },
 })
-
 
 module.exports = mongoose.model('Contact', contactSchema)
